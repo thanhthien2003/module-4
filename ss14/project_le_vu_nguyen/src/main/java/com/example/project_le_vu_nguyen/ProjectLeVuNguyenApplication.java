f4d8e29d2f3dd6f185de09d1d1dac0a0ca3e846f
Admin <vuthanhlong0906438630@gmail.com>
2023-07-04 15:05:41 +0700

    Message

---
+++ ss14/project_le_vu_nguyen/src/main/java/com/example/project_le_vu_nguyen/ProjectLeVuNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_le_vu_nguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeVuNguyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLeVuNguyenApplication.class, args);
+    }
+
+}
---
+++ ss14/project_le_vu_nguyen/src/test/java/com/example/project_le_vu_nguyen/ProjectLeVuNguyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_le_vu_nguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLeVuNguyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
